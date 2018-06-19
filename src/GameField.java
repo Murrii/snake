6d07df4b99de90d6e44b7b39a65613609cc30f20
Karina Trofimova <karinatrofimova@gmail.com>
2018-06-19 13:46:47 +0200

    добавлена функция паузы

--- src/GameField.java
+++ src/GameField.java
@@ -6,10 +6,10 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.Random;
 
-public class GameField extends JPanel implements ActionListener{
+public class GameField extends JPanel implements ActionListener {
 
     private final int SIZE = 320;
-    private  final int DOT_SIZE = 16;
+    private final int DOT_SIZE = 16;
     private final int ALL_DOTS = 400;
     private Image dot;
     private Image apple;
@@ -17,7 +17,7 @@ public class GameField extends JPanel implements ActionListener{
     private int appleX;
     private int appleY;
     private int[] x = new int[ALL_DOTS];
-    private int[] y = new int [ALL_DOTS];
+    private int[] y = new int[ALL_DOTS];
     private int dots;
     private Timer timer;
 
@@ -28,14 +28,14 @@ public class GameField extends JPanel implements ActionListener{
     private boolean down = false;
     //
     private boolean inGame = false;
+    private boolean timerOn = false;
 
 
-    public GameField(){
+    public GameField() {
         setBackground(Color.BLACK);
         loadImages();
         addKeyListener(new inGameListener());
         initGame();
-        startTimer();
         setFocusable(true);
     }
 
@@ -46,15 +46,18 @@ public class GameField extends JPanel implements ActionListener{
         down = false;
 
         inGame = true;
+        timerOn = true;
 
         dots = 3;
         for (int i = 0; i < dots; i++) {
             x[i] = 48 - i * DOT_SIZE;
             y[i] = 48;
         }
+
+        startTimer();
     }
 
-    public void startTimer(){
+    public void startTimer() {
         timer = new Timer(250, this);
         timer.start();
         createApple();
@@ -62,19 +65,19 @@ public class GameField extends JPanel implements ActionListener{
 
     public void createApple() {
         newApple();
-        for( int i = dots; i > 0; i--){
-            if( x[i] == appleX  && y[i] == appleY){
+        for (int i = dots; i > 0; i--) {
+            if (x[i] == appleX && y[i] == appleY) {
                 newApple();
             }
         }
     }
 
-    public void newApple(){
+    public void newApple() {
         appleX = new Random().nextInt(20) * DOT_SIZE;
         appleY = new Random().nextInt(20) * DOT_SIZE;
     }
 
-    public void loadImages(){
+    public void loadImages() {
         ImageIcon dotImage = new ImageIcon("field.png");
         dot = dotImage.getImage();
         ImageIcon appleImage = new ImageIcon("apple.png");
@@ -83,36 +86,36 @@ public class GameField extends JPanel implements ActionListener{
         field = fieldImage.getImage();
     }
 
-    public void checkApple(){
-        if(x[0] == appleX && y[0] == appleY){
+    public void checkApple() {
+        if (x[0] == appleX && y[0] == appleY) {
             dots++;
             createApple();
         }
     }
 
-    public void move(){
-        for (int i = dots; i > 0; i--){
-            x[i] = x[i-1];
-            y[i] = y[i-1];
+    public void move() {
+        for (int i = dots; i > 0; i--) {
+            x[i] = x[i - 1];
+            y[i] = y[i - 1];
         }
-        if(left){
-            x[0] -=DOT_SIZE;
+        if (left) {
+            x[0] -= DOT_SIZE;
             if (x[0] < 0)
                 inGame = false;
         }
-        if(right){
-            x[0] +=DOT_SIZE;
-            if (x[0] > (SIZE - DOT_SIZE) )
-                    inGame = false;
-        }
-        if(up){
-            y[0] -=DOT_SIZE;
-            if(y[0] < 0)
+        if (right) {
+            x[0] += DOT_SIZE;
+            if (x[0] > (SIZE - DOT_SIZE))
                 inGame = false;
         }
-        if(down){
-            y[0] +=DOT_SIZE;
-            if(y[0] > (SIZE - DOT_SIZE))
+        if (up) {
+            y[0] -= DOT_SIZE;
+            if (y[0] < 0)
+                inGame = false;
+        }
+        if (down) {
+            y[0] += DOT_SIZE;
+            if (y[0] > (SIZE - DOT_SIZE))
                 inGame = false;
         }
     }
@@ -120,36 +123,34 @@ public class GameField extends JPanel implements ActionListener{
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        if(inGame) {
+        if (inGame) {
             g.drawImage(field, 0, 0, this);
             g.drawImage(apple, appleX, appleY, this);
             for (int i = 0; i < dots; i++) {
                 g.drawImage(dot, x[i], y[i], this);
             }
-        }
-        else {
+        } else {
             String theEnd = "Game Over";
             Font font = new Font("Arial", Font.BOLD, 14);
             g.setColor(Color.LIGHT_GRAY);
             g.setFont(font);
-            g.drawString(theEnd, 120, SIZE/2);
+            g.drawString(theEnd, 120, SIZE / 2);
         }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(inGame){
+        if (inGame) {
             checkBorders();
             checkApple();
             move();
             repaint();
         }
-        //repaint();
     }
 
     public void checkBorders() {
-        for(int i = dots; i > 0; i--){
-            if(x[0] == x[i] && y[0] == y[i])
+        for (int i = dots; i > 0; i--) {
+            if (x[0] == x[i] && y[0] == y[i])
                 inGame = false;
         }
     }
@@ -182,8 +183,17 @@ public class GameField extends JPanel implements ActionListener{
                 down = true;
                 left = false;
             }
-            if (key == KeyEvent.VK_ENTER){
+            if (key == KeyEvent.VK_ENTER) {
                 initGame();
+            }
+            if (key == KeyEvent.VK_SPACE) {
+                if (timerOn) {
+                    timer.stop();
+                    timerOn = false;
+                } else {
+                    timerOn = true;
+                    timer.start();
+                }
             }
         }
     }
